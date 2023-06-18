b157cc95713dac84721f5aa14257e6ee811af093
0w3w0 <41020733@qq.com>
2023-06-18 11:17:05 +0800

    🦄 refactor: select->map

---
+++ test/slice/distinct_test.go
@@ -0,0 +1,44 @@
+package linq
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDistinct(t *testing.T) {
+	var items = []int{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}
+	var result = Distinct(items)
+	if !reflect.DeepEqual(result, []int{1, 2, 3, 4, 5}) {
+		t.Fail()
+	}
+}
+
+func TestDistinctObject(t *testing.T) {
+	type Item struct {
+		Name  string
+		Value int
+	}
+	var items = []Item{
+		{"A", 1},
+		{"B", 2},
+		{"C", 3},
+		{"D", 4},
+		{"E", 5},
+		{"A", 1},
+		{"B", 2},
+		{"C", 3},
+		{"D", 4},
+		{"E", 5},
+	}
+	var result = Distinct(items)
+	if !reflect.DeepEqual(result, []Item{
+		{"A", 1},
+		{"B", 2},
+		{"C", 3},
+		{"D", 4},
+		{"E", 5},
+	}) {
+		t.Fail()
+	}
+	t.Logf("%+v", result)
+}
